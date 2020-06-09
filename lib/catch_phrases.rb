b11e773f4d094a24ca302ddedf2d2e0755669f79
Jacinta Calixte <jacinta246@yahoo.com>
2020-06-09 22:21:51 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -16,9 +16,9 @@ end
 
 def all_phrases
   phrase = "It's-a me, Mario!"
-puts phrase
-status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-puts status
-puts "It's Dangerous To Go Alone! Take This."
+   puts phrase
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+   puts status
+   puts "It's Dangerous To Go Alone! Take This."
 "It's Dangerous To Go Alone! Take This."
 end
